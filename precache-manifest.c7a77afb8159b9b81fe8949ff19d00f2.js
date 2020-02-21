eaebdf453fa2908a2f1a2afa2720e5920a370409
Anbu Selvan <selvaanb@Anbus-MacBook-Pro.local>
2020-02-21 21:12:06 +0400

    Updates

---
+++ precache-manifest.c7a77afb8159b9b81fe8949ff19d00f2.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "100b0c3fa275e1339ef8a711a3754677",
+    "url": "/greencommunication/index.html"
+  },
+  {
+    "revision": "11a485932ffc7cd0c7dc",
+    "url": "/greencommunication/static/css/2.cf674a49.chunk.css"
+  },
+  {
+    "revision": "95f49e551203760015b7",
+    "url": "/greencommunication/static/css/main.72473faa.chunk.css"
+  },
+  {
+    "revision": "11a485932ffc7cd0c7dc",
+    "url": "/greencommunication/static/js/2.d5be35f7.chunk.js"
+  },
+  {
+    "revision": "95f49e551203760015b7",
+    "url": "/greencommunication/static/js/main.8b85c511.chunk.js"
+  },
+  {
+    "revision": "40c77cfefd4b3a523d8c",
+    "url": "/greencommunication/static/js/runtime~main.4d674c82.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/greencommunication/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/greencommunication/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/greencommunication/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/greencommunication/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/greencommunication/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/greencommunication/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "9b3e4f7f02aa277885fb344ee25de7b7",
+    "url": "/greencommunication/static/media/headerlogo.9b3e4f7f.jpg"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/greencommunication/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/greencommunication/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/greencommunication/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/greencommunication/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/greencommunication/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/greencommunication/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/greencommunication/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/greencommunication/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/greencommunication/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/greencommunication/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
